4b976ee651455879b71549988a7441725caa11a9
Dede Assann <dedeassann20@gmail.com>
2022-04-28 13:53:09 -0400

    Jai encore apporter une serie de modification au programme et jai eu une moyenne de 9,67 pour pylint!

--- energie_mecanique.py
+++ energie_mecanique.py
@@ -2,6 +2,8 @@
 personne quelconque.
 """
 import matplotlib.pyplot as plt
+
+
 def energie_mecanique() -> float:
     """Cette fonction vous permet de caculer l'energie
     mecanique d'un systeme. Elle vous demande tout d'abord
@@ -14,17 +16,16 @@ def energie_mecanique() -> float:
     vitesse = float(input("Entrez la vitesse de l'objet en metre par seconde: "))
     force_g = 9.81  # ATTRACTION DE LA PESANTEUR en Newton par Kilogramme
     if vitesse == 0:
-        energie_potentielle = masse * force_g *hauteur
+        energie_potentielle = masse * force_g * hauteur
         print(f"L'energie potentielle du systeme est {energie_potentielle} Joules.")
     elif hauteur == 0:
-        energie_cinetique = (masse * (vitesse**2)) /2
+        energie_cinetique = (masse * (vitesse**2)) / 2
         print(f"L'energie cinetique du systeme est {energie_cinetique} Joules.")
     else:
-        e_mecan = (masse*(vitesse**2))/2+masse * force_g *hauteur
+        e_mecan = (masse * (vitesse**2)) / 2 + masse * force_g * hauteur
         print(f"L'energie mecanique du systeme est {e_mecan} Joules.")
 
-    
-    ask = str(input("Voulez-vous un graphique du systeme?: "))
+    ask = input("Voulez-vous un graphique du systeme?: ")
     if ask in ("y", "yes", "oui"):
         """Cette partie de la fonction permet de construire un graphe en
         fonction des donnees de la premiere partie, mais pour cela il va devoir
@@ -41,16 +42,15 @@ def energie_mecanique() -> float:
         e_mecan = []  # liste d'energie mecanique
         altitude = [hauteur]  # liste d'altitude
         celerite = [vitesse]  # liste de vitesseitesse
-        ask2 = str(
-            input(
-                "Voulez-vous avoir un grapre pour l'energie potentielle de l'objet?"
-            )
+        reponse = ["y","yes","oui"]
+        ask2 = input(
+            "Voulez-vous avoir un grapre pour l'energie potentielle de l'objet?"
         )
-        ask3 = str(
-            input("Voulez-vous avoir un grape pour l'energie cinetique de l'objet?  ")
+        ask3 = input(
+            "Voulez-vous avoir un grape pour l'energie cinetique de l'objet?  "
         )
-        ask4 = str(
-            input("Voulez-vous avoir un graphe pour l'energie mecanique de l'objet?  ")
+        ask4 = input(
+            "Voulez-vous avoir un graphe pour l'energie mecanique de l'objet?  "
         )
         demand = int(
             input(
@@ -59,24 +59,28 @@ def energie_mecanique() -> float:
             )
         )
         temps = []  # liste de temps
-        temps1 =0
+        temps1 = 0
         while temps1 <= demand:
             temps.append(temps)
-            temps1 +=1
+            temps1 += 1
 
-        if ask2 in ("y", "yes", "oui"):
+        if ask2 in reponse:
 
             def graph_epp():
-                avancement =0
+                avancement = 0
                 while avancement < demand:
-                    avancement +=1
-                    hauteur1 = int(input("Entrez l'altitude de l'objet par rapport au sol: "))
+                    avancement += 1
+                    hauteur1 = int(
+                        input("Entrez l'altitude de l'objet par rapport au sol: ")
+                    )
                     altitude.append(hauteur1)
                 print(f"Voici la liste des altitudes donnee: {altitude}.")
                 for k in altitude:
-                    energie_potentielle = masse * force_g *k
+                    energie_potentielle = masse * force_g * k
                     e_potent.append(energie_potentielle)
-                print(f"Voici la liste des energies potentielles calculees: {e_potent}.")
+                print(
+                    f"Voici la liste des energies potentielles calculees: {e_potent}."
+                )
                 plt.plot(temps, e_potent, "b-", lw=4)
                 plt.title(
                     "Evolution de l'energie potentielle de pesanteur en fonction du temps"
@@ -91,17 +95,17 @@ def energie_mecanique() -> float:
             if __name__ == "__main__":
                 graph_epp()
 
-        if ask3 in ("y", "yes", "oui"):
+        if ask3 in reponse:
 
             def graph_ec():
-                avancement =0
+                avancement = 0
                 while avancement < demand:
-                    avancement +=1
+                    avancement += 1
                     vitesse1 = int(input("Entrez la vitesse de l'objet: "))
                     celerite.append(vitesse1)
                 print(f"Voici la liste de vitesses donnee: {celerite}.")
                 for i in celerite:
-                    energie_cinetique = (masse * (i**2)) /2
+                    energie_cinetique = (masse * (i**2)) / 2
                     e_cine.append(energie_cinetique)
                 print(f"Voici la liste des energies cinetiques calculees: {e_cine}.")
                 plt.plot(temps, e_cine, "g-", lw=4)
@@ -116,7 +120,7 @@ def energie_mecanique() -> float:
             if __name__ == "__main__":
                 graph_ec()
 
-        if ask4 in ("y", "yes", "oui"):
+        if ask4 in reponse:
             for k in range(demand + 1):
                 e_mecan = e_potent[k] + e_cine[k]
                 e_mecan.append(energie_mecanique)
@@ -126,7 +130,7 @@ def energie_mecanique() -> float:
             plt.rcParams["figure.figsize"] = (10, 8)
             plt.xlabel("Temps")
             plt.ylabel("Energie mecanique")
-            plt.grid("equal", axis="bothauteur")
+            plt.grid("equal", axis="both")
             plt.show()
             plt.close()
     else:
