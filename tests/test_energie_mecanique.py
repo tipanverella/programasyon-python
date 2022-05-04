1e35ea277da15e380a5c669033a9bc902083f16c
Tipan Verella <tipan.verella@gmail.com>
2022-05-04 11:31:57 -0400

    test_transfome_struktu_done

--- tests/test_energie_mecanique.py
+++ tests/test_energie_mecanique.py
@@ -1,4 +1,4 @@
-from energie_mecanique import energie_mecanique, GRAVITE
+from energie_mecanique import energie_mecanique, GRAVITE, transfome_struktu_done
 
 
 def test_energie_mecanique():
@@ -29,3 +29,34 @@ def test_energie_mecanique():
     tip_eneji, kantite_eneji = energie_mecanique(masse, hauteur, vitesse)
     assert kantite_eneji == 0
     assert tip_eneji == "Energie Mecanique"
+
+
+def test_transfome_struktu_done():
+    ell = [(1.0, 2.0, 3.0)]
+    idx, masse, hauteur, vitesse = transfome_struktu_done(ell)
+    assert idx == [
+        0,
+    ]
+    assert masse == [
+        1.0,
+    ]
+    assert hauteur == [
+        2.0,
+    ]
+    assert vitesse == [
+        3.0,
+    ]
+
+    ell = [(1.0, 2.0, 3.0), (7.0, 8.0, 9.0), (4.0, 5.0, 6.0)]
+    idx, masse, hauteur, vitesse = transfome_struktu_done(ell)
+    assert idx == [0, 1, 2]
+    assert masse == [1.0, 7.0, 4.0]
+    assert hauteur == [2.0, 8.0, 5.0]
+    assert vitesse == [3.0, 9.0, 6.0]
+
+    ell = []
+    idx, masse, hauteur, vitesse = transfome_struktu_done(ell)
+    assert idx == []
+    assert masse == []
+    assert hauteur == []
+    assert vitesse == []
