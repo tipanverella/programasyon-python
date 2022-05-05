928185263abc20223510a26b001df9750e7722dc
Dede Assann <dedeassann20@gmail.com>
2022-05-05 18:53:32 -0400

    changing abt the issues

--- energie_mecanique/__init__.py
+++ energie_mecanique/__init__.py
@@ -43,79 +43,50 @@ def energie_mecanique(
     return tip_eneji, kantite_eneji
 
 
-def li_dokuman(file_path: str) -> List[Tuple[float, float, float]]:
+def li_dokuman(file_path:str) -> List[Tuple[float, float, float]]:
     """
     Fonksyon sa a li done ki lan dokuman an
     """
     with open(file_path, "r", encoding="utf-8") as csv_file:
         reader = csv.DictReader(csv_file)
-        return [
+        rangee = [
             (float(row["masse"]), float(row["hauteur"]), float(row["vitesse"]))
             for row in reader
         ]
+        return rangee
 
 
-def transfome_struktu_done(liste_triplet: List[Tuple[float, float, float]]):
+def transfome_struktu_done() -> List[Tuple[float, float, float]]:
     """
     fonksyon sa a,  transfome yon list triple an 4 list ki gen menm longe ak
     list triple a
     """
-    return liste_triplet
+    rangee = li_dokuman()
+    transposed = [[row[i] for row in rangee] for i in range(4)]
+    masse = [transposed[0]]
+    hauteur = [transposed[1]]
+    vitesse = [transposed[3]]
+    return masse, hauteur, vitesse
 
 
-altitude = []  # liste d'altitude
-celerite = []  # liste de vitesse
-
-
-def recueil_donne():
-    """Cette partie de la fonction permet de construire un graphe en
-    fonction des donnees de la premiere partie, mais pour cela il va devoir
-    vous demander des informations supplementaires, qui lui permettront de
-    de calculer des valeurs d'energies supplementaires, afin d'en creer
-    une liste et ainsi faire le graphique.
-    Les axes a representer seront l'axe des abscisses sur lequel sera
-    represente l'evolution du temps, et l'axe des ordonnees, qui
-    representera les energies en question.
+def calcul_pour_etude_systeme():
+    """Cette fonction effectue les calculs sur les valeurs donnes pour la
+    masse, la hauteur et la vitesse de l'objet en question et renvoie des
+    listes d'energie potentielle, cinetique et mecanique.
     """
-    demand = int(
-        input(
-            "Combien de valeurs voulez-vous donner pour l'altitude"
-            " et la vitesse de l'objet? : "
-        )
-    )
-    e_potent = []  # liste d'energie potentielle
-    e_cine = []  # liste d'energie cinetique
-    e_mecan = []  # liste d'energie mecanique
-    temps = []  # liste de temps
-    temps1 = 0
-    while temps1 < demand:
-        temps.append(temps1)
-        temps1 += 1
-    masse = float(input("Donner la valeur de la masse de l'objet en Kilogramme : "))
-    avancement = 0
-    while avancement < demand:
-        avancement += 1
-        hauteur1 = float(input("Entrez l'altitude de l'objet par rapport au sol : "))
-        altitude.append(hauteur1)
-        vitesse1 = float(input("Entrez la vitesse de l'objet : "))
-        celerite.append(vitesse1)
-    for i in altitude:
-        kantite_eneji = masse * GRAVITE * i
-        e_potent.append(kantite_eneji)
-    for i in celerite:
-        kantite_eneji = (masse * (i**2)) / 2
-        e_cine.append(kantite_eneji)
-    for i in range(demand):
-        kantite_eneji = e_potent[i] + e_cine[i]
-        e_mecan.append(kantite_eneji)
-    return temps, e_potent, e_cine, e_mecan
+    masse, hauteur, vitesse = transfome_struktu_done()
+    e_potent = [(i * GRAVITE * k) for i in masse for k in hauteur]
+    e_cine = [(i * (v**2) / 2) for i in masse for v in vitesse]
+    e_mecan = [(i + g) for i in e_potent for g in e_cine]
+    return e_potent, e_cine, e_mecan
 
 
-def etude_syst():
+def etude_systeme():
     """
     Cette fonction construis des graphiques.
     """
-    temps, e_potent, e_cine, e_mecan = recueil_donne()
+    e_potent, e_cine, e_mecan = calcul_pour_etude_systeme()
+    temps = [list(range(e_mecan))]
     graph_ep = plt.plot(temps, e_potent, "b-", lw=4)
     graph_ec = plt.plot(temps, e_cine, "g-", lw=4)
     graph_em = plt.plot(temps, e_mecan, "k-", lw=4)
@@ -163,7 +134,7 @@ def systeme_mecanique():
         print(f"L'{tip_eneji} du systeme est {kantite_eneji} joules.")
     elif chwa == "2":
         # fok li ofri enteraksyon pou konprann yon sistem mekanik
-        graph_ep, graph_ec, graph_em = etude_syst()
+        graph_ep, graph_ec, graph_em = etude_systeme()
         print(graph_ep, graph_ec, graph_em, "Voila vos graphiques! Bonne analyse!")
         print("Energie mecanique en noir,")
         print("Energie cinetique en vert,")
