a05b88304d398b8170782a450123cef108059239
Dede Assann <dedeassann20@gmail.com>
2022-05-09 11:50:23 -0400

    pylint test passed

--- energie_mecanique/__init__.py
+++ energie_mecanique/__init__.py
@@ -58,7 +58,7 @@ def li_dokuman(file_path: str) -> List[Tuple[float, float, float]]:
         ]
 
 
-def reformattage_et_calcul(ell: List[Tuple[float, float, float]]):
+def reformattage(ell: List[Tuple[float, float, float]]):
     """
     fonksyon sa a,  transfome yon list triple an 4 list ki gen menm longe ak
     list triple a.
@@ -66,15 +66,15 @@ def reformattage_et_calcul(ell: List[Tuple[float, float, float]]):
     rangee = zip(ell)
     # changement de l'organisation des donnees
     # du fichier csv en une liste de trois "tuples"...
-    idx = [(idx) for idx in ell]
-    masse = [rangee[0]]
-    hauteur = [rangee[1]]
-    vitesse = [rangee[2]]
+    transposed = [[idx, elem] for idx, elem in rangee]
+    idx = [list(ell)]
+    masse = [transposed[0]]
+    hauteur = [transposed[1]]
+    vitesse = [transposed[2]]
     return idx, masse, hauteur, vitesse
 
-def calcul_energie_pour_graphes(
-    idx: list, masse: list, hauteur: list, vitesse: list
-):
+
+def calcul_energie_pour_graphes(idx: list, masse: list, hauteur: list, vitesse: list):
     """
     ette fonction effectue les calculs sur les valeurs donnees pour la
     masse, la hauteur et la vitesse de l'objet en question et renvoie des
@@ -88,9 +88,7 @@ def calcul_energie_pour_graphes(
     return idx, e_potent, e_cine, e_mecan
 
 
-def etude_systeme(
-    idx: list, e_potent: list, e_cine: list, e_mecan:list
-):
+def etude_systeme(idx: list, e_potent: list, e_cine: list, e_mecan: list):
     """
     Cette fonction construis des graphiques.
     """
@@ -141,9 +139,12 @@ def systeme_mecanique():
         print(f"L'{tip_eneji} du systeme est {kantite_eneji} joules.")
     elif chwa == "2":
         # fok li ofri enteraksyon pou konprann yon sistem mekanik
-        liste_triplet = li_dokuman()
-        masse, hauteur, vitesse = reformattage_et_calcul(ell=)
-        graph_ep, graph_ec, graph_em = etude_systeme()
+        # pylint: disable = no-value-for-parameter   # Disables the pylint check for the next line
+        idx, masse, hauteur, vitesse = reformattage()
+        idx, e_potent, e_cine, e_mecan = calcul_energie_pour_graphes(
+            idx, masse, hauteur, vitesse
+        )
+        graph_ep, graph_ec, graph_em = etude_systeme(idx, e_potent, e_cine, e_mecan)
         print(graph_ep, graph_ec, graph_em, "Voila vos graphiques! Bonne analyse!")
         print("Energie mecanique en noir,")
         print("Energie cinetique en vert,")
