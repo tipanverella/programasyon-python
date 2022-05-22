b7b4210bcd7bd5b58cc40ea3ca4c5e0957140f9d
Dede Assann <dedeassann20@gmail.com>
2022-05-21 21:43:12 -0400

    changes

--- graph_function.py
+++ graph_function.py
@@ -1,4 +1,3 @@
-from typing import List
 from matplotlib import pyplot as plt
 
 def graphe(x = [], y = []):
@@ -11,14 +10,33 @@ def graphe(x = [], y = []):
     title = input("Donnez le titre de votre graphique: ")
     x_label = input("Ki tit axe abscisse ou an: ")
     y_label  = input("Ki tit axe ordonnee ou an: ")
-    graph = plt.plot(x, y, "k-", lw=2.5)
+
+    pas1 = x[-1] / 100
+    value1 = x[0]
+    abscisse = [value1]
+    while value1 != x[-1]:
+        value1 += pas1
+        abscisse.append(value1)
+
+    pas2 = y[-1]/ 100
+    value2 = y[0]
+    ordonnee = [value2]
+    while value2 != y[-1]:
+        value2 += pas2
+        ordonnee.append(value2)
+
+    plt.plot(abscisse, ordonnee, "k-", lw=2.5)
     plt.title(title)
-    plt.rcParams["figure.figsize"] = (20, 18)
+    plt.rcParams["figure.figsize"] = (10, 8)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    plt.grid("equal", axis="both")
+    plt.grid("equal", axis="both", color="g", lw=2.5)
+    plt.axis("equal" ,xmin=-100, xmax=100, ymin=-100, ymax=100)
+    plt.legend()
     plt.show()
     plt.close()
-    return graph
 
+    
+    
 
+        
--- squares.py
+++ squares.py
@@ -1,21 +1,55 @@
-def squares1(x):
-    """
-    This function calculate the square value of 
-    any number that u give
-    """
-    carre = [i**2 for i in range(x+1)]
-    X = [i for i in range(x+1)]
-    from graph_function import graphe
-    graph = graphe(X, carre)
-    return carre, graph
-
-
 def squares(x):
     """
     fonksyon sa baw yon list de carre tout valeur 
     ki inferieur a sa ou ba li a, rive nan saw bay la.
     """
     return(x**2)
+
+
+
+def squares1(x):
+    """
+    This function calculate the square value of 
+    any number that u give
+    """
+    carre = [i**2 for i in range(x+1)]
+    return carre
+
+
+def squares3(x):
+    """
+    Fonksyon sa aksepte yon grenn argument. Li pranl kom yon valeur
+    maximal de yon serie de valeur ki pati de 0, pou rive nan valeur
+    ou bay la. Li fe kalkil carre yo sou li, li remet ou yon lis de
+    valeur au carre yo, et li bay yon graphe de valeur sa yo.
+    """
+
+    import matplotlib.pyplot as plt
+
+    carre = [i**2 for i in range(x+1)]
+
+    pas_abs = x / 100
+    value_abs = 0
+    abscisse = [value_abs]
+    while value_abs <= x:
+        value_abs += pas_abs
+        abscisse.append(value_abs)
+    
+    abscisse.copy()
+    ordonnee = [i**2 for i in abscisse]
+
+    graph = plt.plot(abscisse, ordonnee, "k-*", lw=2.5)
+    plt.title("Courbe d'evolution de la fonction carree")
+    plt.xlabel("x values")
+    plt.ylabel("squares")
+    plt.grid("equal", axis="both", color="g", lw=2.5)
+    plt.axis("equal" ,xmin=-100, xmax=100, ymin=-100, ymax=100)
+    plt.legend()
+    plt.show()
+    plt.close()
+
+    return carre, graph
+
 
 
 
