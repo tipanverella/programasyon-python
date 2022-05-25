0e2e00729abff4a69c97033f8f4c4714bf05ed28
Dede Assann <dedeassann20@gmail.com>
2022-05-25 09:50:51 -0400

    changes

--- essay.py
+++ essay.py
@@ -1,31 +1,29 @@
-from itertools import zip_longest
-
+"""
+Module dessaie
+"""
+import matplotlib.pyplot as plt
 from energie_mecanique import li_dokuman
 
 lecture = li_dokuman(file_path="/c/Users/dedea/git/programasyon-python")
-transposed = zip_longest[lecture]
+transposed = zip(lecture)
 print(transposed)
 
 
-
-
-import matplotlib.pyplot as plt
-
-a = 1 / 1000 # Pour éviter 0
-b = 1
+VALUE_A = 1 / 1000  # Pour éviter 0
+VALUE_B = 1
 x = []
 y = []
 x1 = []
 y1 = []
-pas = (b - a) / 200
-abscisse = a
-for k in range(0, 201): # On fait une seule boucle
-    x.append(abscisse) # abscisse représente les abscisses à droite de 0
-    y.append(1 / abscisse)
-    x1.append(-abscisse) # -abscisse représente les abscisses à gauche de 0
-    y1.append(-1 / abscisse)
-    abscisse += pas
-plt.axis([-1, 1, -10, 10])
+PAS = (VALUE_B - VALUE_A) / 200
+ABSCISSE = VALUE_A
+for k in range(0, 201):  # On fait une seule boucle
+    x.append(ABSCISSE)  # ABSCISSE représente les abscisses à droite de 0
+    y.append(1 / ABSCISSE)
+    x1.append(-ABSCISSE)  # -ABSCISSE représente les abscisses à gauche de 0
+    y1.append(-1 / ABSCISSE)
+    ABSCISSE += PAS
 plt.plot(x, y, x1, y1)
+plt.axis([-1, 1, -10, 10])
 plt.show()
 plt.close()
