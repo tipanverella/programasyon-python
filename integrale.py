8bf1ba060a9cddd93ac213da33f89b1b6c82da1a
Dede Assann <dedeassann20@gmail.com>
2022-06-12 13:35:46 -0400

    changes

--- integrale.py
+++ integrale.py
@@ -3,6 +3,7 @@ Module sa pemet ou kalkule integrale par metod simpson ak trapeze.
 """
 
 
+from cmath import cos
 from dataclasses import dataclass
 from typing import Tuple
 
@@ -12,10 +13,19 @@ class functions():
     coeff: float
 
 
-    def find_expr():
+    def find_expr(y):
         "Fonksyon sa aprann rekonet expression"
 
+        import math
+        from math import pi
         
+        if True:
+            a= y.count(cos)
+            b = y.count(pi)
+            c = y.count("x")
+            if a is not None:
+                replace
+
 
 
 def interval(interval:Tuple, nonmb_enteval:int):
@@ -25,3 +35,12 @@ def interval(interval:Tuple, nonmb_enteval:int):
 
     pas = (interval[1]-interval[0])/nonmb_enteval
     y = input("entre equation fonction ou lan ")
+
+
+exp_func  = input("antre expression fonction ou an... ")
+
+try:
+    exp_func = eval(exp_func)
+except NameError:
+    "Nap vanse!"
+
