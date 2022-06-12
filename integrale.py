816990f17421c2974cbe65a43e7c53c5c50bb785
Dede Assann <dedeassann20@gmail.com>
2022-06-12 13:14:13 -0400

    changes

---
+++ integrale.py
@@ -0,0 +1,27 @@
+"""
+Module sa pemet ou kalkule integrale par metod simpson ak trapeze.
+"""
+
+
+from dataclasses import dataclass
+from typing import Tuple
+
+
+@dataclass
+class functions():
+    coeff: float
+
+
+    def find_expr():
+        "Fonksyon sa aprann rekonet expression"
+
+        
+
+
+def interval(interval:Tuple, nonmb_enteval:int):
+    """
+    fonksyon sa 
+    """
+
+    pas = (interval[1]-interval[0])/nonmb_enteval
+    y = input("entre equation fonction ou lan ")
